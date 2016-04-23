0faf509a46ce89c9215b7d31b7bbaa1877de85ec
nash <preflight.cc@gmail.com>
2016-04-23 20:57:49 +0900

    dispatch an event when `recording.json` changed

--- src/handlers/recording.js
+++ src/handlers/recording.js
@@ -1,5 +1,13 @@
+import _ from 'lodash';
+
 const recording = (curr, prev) => {
 	const data = JSON.parse(load(paths.recording));
+
+	if (_.isEmpty(data)) {
+		return;
+	}
+
+	dispatcher.emit('recording');
 };
 
 export default recording;
