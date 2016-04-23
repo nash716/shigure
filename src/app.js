ca31305ef52d98ffc53a5ce723e0806b11818751
nash <preflight.cc@gmail.com>
2016-04-23 20:55:50 +0900

    create dispatcher

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
+// setup global variables
+
 import fs from 'fs';
-import handlers from './handlers';
+import { EventEmitter } from 'events';
 
 global.load = (path) => fs.readFileSync(path).toString();
 
-global.config = JSON.parse(load(__dirname + '/../data/config.json'));
+global.dispatcher = new EventEmitter();
 global.config = JSON.parse(load(`${__dirname}/../data/config.json`));
 
 global.paths = {
 	recorded: `${config.chinachu}/recorded.json`,
 	recording: `${config.chinachu}/recording.json`
 };
+
+// register handlers
+
+// browserify が import を先頭に持ってくるので、global にエクスポートしたものがモジュールで読めずエラーになる
+// ちょっと嫌だけどこの書き方なら大丈夫なので。。。
+const handlers = require('./handlers')['default'];
 
 fs.watchFile(paths.recorded, handlers.recorded);
 fs.watchFile(paths.recording, handlers.recording);
